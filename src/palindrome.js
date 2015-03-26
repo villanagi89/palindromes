e4c4dcb17beee07363f5d9ec76d09aa3ca944eab
Gina Villarreal <gina.nvillarreal@gmail.com>
2015-03-26 09:29:44 -0400

    created functions to pass two tests

--- src/palindrome.js
+++ src/palindrome.js
@@ -1,1 +1,13 @@
 var palindrome = {};
+
+function palidrome(string){
+  // check to see if something is reversed.
+  return string.split('').reversed;
+
+//check if there are two palindrome
+  function count(arr){
+    for (var i=0; i < arr.length; i++){
+      palindrome.push(arr[i] ==reverse(arr[i]));
+    }
+    return palindrome;
+}
