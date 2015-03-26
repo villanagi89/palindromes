575107c5da520c4eed9498db6a0e7b7db4e12997
Daniel Johnson <dan@balancerockmedia.com>
2015-03-26 08:28:36 -0400

    init

---
+++ spec/palindrome-spec.js
@@ -0,0 +1,17 @@
+describe('palidrome', function() {
+    describe('count(arr)', function() {
+        it('phrases should contain 2 palidromes', function() {
+
+            var phrases = ['JavaScript is fun', 'Evil olive', 'Never odd or even'];
+
+            expect(palindrome.count(phrases)).toEqual(2);
+        });
+
+        it('phrases should not contain 3 palidromes', function() {
+
+            var phrases = ['JavaScript is fun', 'Evil olive', 'Never odd or even'];
+
+            expect(palindrome.count(phrases)).not.toEqual(3);
+        });
+    });
+});
